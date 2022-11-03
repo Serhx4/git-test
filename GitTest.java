28a0bfbeda9106e2b2ef890e596e8a358a90db26
Serhx4 <111274705+Serhx4@users.noreply.github.com>
2022-11-03 13:53:49 +0200

    Update implementation of introduce from home

--- GitTest.java
+++ GitTest.java
@@ -1,9 +1,8 @@
 public class GitTest {
 
-
-	
 	public static void main (String[] args) {
 		System.out.println("Test git repository");
+		introduce("Serhx4");
 	}
 
 	private static void introduce(String name) {
